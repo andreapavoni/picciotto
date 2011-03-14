0c5b8c8df0679e1d9109a7d0c88b964123a95218
Andrea Pavoni <apeacox@gmail.com>
2011-03-14 14:16:16 +0100

    small refactoring to main controller

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -7,7 +7,7 @@ Picciotto.controllers  do
     permalink = params[:permalink]
     path = "#{PADRINO_ROOT}/app/views/main/static/#{permalink}.haml"
     not_found if permalink.match(/\.\./) or permalink.match(/\//) or permalink.match(/^layouts$/) or !File.exist?(path)
-    render "main/#{permalink}"
+    render "main/static/#{permalink}"
   end
 
   not_found do
